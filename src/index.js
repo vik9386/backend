c686abdab11ac63e861a4f6acf8a92c3f7e95eab
vik9386 <kmr.vikash1994@gmail.com>
2024-07-20 23:50:55 +0530

    middleware cookie parser and cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose"
 // import {DB_NAME} from "constants.js"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path:"./env"
 })
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(`MongoDB connection failed !!! ${err}`)
+})
 
 /*
 import express from "express"
