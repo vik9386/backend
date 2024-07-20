1d86c7174bd6dff7ef90784f90296983afed8da0
vik9386 <kmr.vikash1994@gmail.com>
2024-07-20 23:00:59 +0530

    connect database

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoos from "mongoose"
+import { DB_NAME } from "../constatnts.js"
+
+
+const connectDB = async ()=>{
+    try {
+        const connectionINstance =  await mongoos.connect(`${process.env.MONGODB_URI}/{DB_NAME}`)
+        console.log(`\n MONGODB Connected !! DB Host : ${connectionINstance.connection.host}`)
+    } catch (error) {
+        console.error("MONGODB connection error : ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
+
